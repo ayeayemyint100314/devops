ba242a768b0e89e9331607fd886428843b168ff3
ayeayemyint100314 <ayeayemyint0709@gmail.com>
2024-09-27 16:02:20 +0630

    sql world is tested and ok

---
+++ src/main/java/com/napier/sem/Country.java
@@ -0,0 +1,51 @@
+package com.napier.sem;
+
+public class Country {
+
+private int id;
+private String name;
+private String countryCode;
+private String district;
+private long population;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public long getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(long population) {
+        this.population = population;
+    }
+
+}
